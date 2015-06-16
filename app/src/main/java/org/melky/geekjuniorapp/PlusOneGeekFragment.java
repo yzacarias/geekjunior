6ea3c435d9d76449ad8dde534d348f7c5eeea3ca
RROMERO <yzacarias@gmail.com>
2015-06-16 18:28:23 +0200

    Finalizada la navegacion de fragments 2

--- app/src/main/java/org/melky/geekjuniorapp/PlusOneGeekFragment.java
+++ app/src/main/java/org/melky/geekjuniorapp/PlusOneGeekFragment.java
@@ -1,5 +1,6 @@
 package org.melky.geekjuniorapp;
 
+import android.app.Activity;
 import android.app.Notification;
 import android.content.Intent;
 import android.net.Uri;
@@ -10,6 +11,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
+import com.google.android.gms.analytics.HitBuilders;
+import com.google.android.gms.analytics.Tracker;
 import com.google.android.gms.plus.PlusOneButton;
 
 /**
@@ -19,6 +22,19 @@ public class PlusOneGeekFragment extends Fragment {
     private PlusOneButton p;
     private static final String URL = "https://plus.google.com/+GeekjuniorFr";
     private static final int PLUS_ONE_REQUEST_CODE = 0;
+    private GeekJunior act;
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        act = (GeekJunior) activity;
+
+        if(GoogleAnalyticsApp.googleAnalitic) {
+            Tracker t = ((GoogleAnalyticsApp) act.getApplication()).getTracker(GoogleAnalyticsApp.TrackerName.APP_TRACKER);
+            t.setScreenName("PlusOneGeekFragment");
+            t.send(new HitBuilders.ScreenViewBuilder().build());
+        }
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -28,6 +44,15 @@ public class PlusOneGeekFragment extends Fragment {
         fb.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if(GoogleAnalyticsApp.googleAnalitic) {
+                    Tracker t = ((GoogleAnalyticsApp) act.getApplication()).getTracker(GoogleAnalyticsApp.TrackerName.APP_TRACKER);
+                    t.setScreenName("PlusOneGeekFragment");
+                    t.send(new HitBuilders.SocialBuilder()
+                            .setNetwork("Facebook")
+                            .setAction("click")
+                            .setTarget("SOME_URL")
+                            .build());
+                }
                 Intent myIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/geekjunior"));
                 startActivity(myIntent);
             }
@@ -37,6 +62,15 @@ public class PlusOneGeekFragment extends Fragment {
         tw.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if(GoogleAnalyticsApp.googleAnalitic) {
+                    Tracker t = ((GoogleAnalyticsApp) act.getApplication()).getTracker(GoogleAnalyticsApp.TrackerName.APP_TRACKER);
+                    t.setScreenName("PlusOneGeekFragment");
+                    t.send(new HitBuilders.SocialBuilder()
+                            .setNetwork("Twitter")
+                            .setAction("click")
+                            .setTarget("SOME_URL")
+                            .build());
+                }
                 Intent myIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://twitter.com/geekjuniorfr"));
                 startActivity(myIntent);
             }
@@ -46,6 +80,15 @@ public class PlusOneGeekFragment extends Fragment {
         gp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if(GoogleAnalyticsApp.googleAnalitic) {
+                    Tracker t = ((GoogleAnalyticsApp) act.getApplication()).getTracker(GoogleAnalyticsApp.TrackerName.APP_TRACKER);
+                    t.setScreenName("PlusOneGeekFragment");
+                    t.send(new HitBuilders.SocialBuilder()
+                            .setNetwork("Google+")
+                            .setAction("click")
+                            .setTarget("SOME_URL")
+                            .build());
+                }
                 Intent myIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://plus.google.com/+GeekjuniorFr/posts"));
                 startActivity(myIntent);
             }
