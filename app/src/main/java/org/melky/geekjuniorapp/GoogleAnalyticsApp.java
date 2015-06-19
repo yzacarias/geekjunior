d0d5384cf6d8bc18d50c9d6044959b8e727b927a
RROMERO <yzacarias@gmail.com>
2015-06-19 08:58:34 +0200

    Vaciar codigo de analytic

--- app/src/main/java/org/melky/geekjuniorapp/GoogleAnalyticsApp.java
+++ app/src/main/java/org/melky/geekjuniorapp/GoogleAnalyticsApp.java
@@ -15,7 +15,7 @@ import java.util.HashMap;
 public class GoogleAnalyticsApp extends Application {
     public static boolean googleAnalitic;
     // change the following line
-    private static final String PROPERTY_ID = "UA-62994104-2";
+    private static final String PROPERTY_ID = "XX-XXXXXXXX-X";
 
 
     public static int GENERAL_TRACKER = 0;
